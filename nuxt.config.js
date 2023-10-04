300419d6a37979e141539bad455ddc5350bb887d
Jose Luis Gimenez <joseviedmacorreo@gmail.com>
2023-10-03 21:09:12 -0300

    host2

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ export default {
 
   server: {
     port: 3000, // default: 3000
-    host: '3.12.201.168', // default: localhost
+    host: '0.0.0.0', // default: localhost
   },
 
   serverMiddleware: {
